cf5056eae86cb0f2e4fceef67b56f42b8f05b586
Alexandr Mironenko <sasha9.mironenko@yandex.ru>
2019-02-28 22:39:54 +0200

    Update header.blade.php

--- resources/views/blocks/header.blade.php
+++ resources/views/blocks/header.blade.php
@@ -1,6 +1,6 @@
 <header>
   <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="{{URL('/')}}">Позитивчик))))<img  src="{{ asset('images/happy.png') }}" class="hnavim" alt="" ></a>
+  <a class="navbar-brand" href="{{secure_url('/')}}">Позитивчик))))<img  src="{{ secure_asset('images/happy.png') }}" class="hnavim" alt="" ></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
